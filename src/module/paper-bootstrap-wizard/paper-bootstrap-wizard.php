75f30d723de7007ebe3fcdf006bf35a70878142f
maxsv0 <max.svistunov@gmail.com>
2018-06-13 21:12:02 +0200

    Paper Bootstrap Wizard

---
+++ src/module/paper-bootstrap-wizard/paper-bootstrap-wizard.php
@@ -0,0 +1,10 @@
+<?php
+
+// Include CSS for Paper Bootstrap Wizard
+msv_include(CONTENT_URL."/css/paper-bootstrap-wizard.css");
+
+// Include JS for Paper Bootstrap Wizard
+msv_include(CONTENT_URL."/js/jquery.bootstrap.wizard.js");
+msv_include(CONTENT_URL."/js/paper-bootstrap-wizard.js");
+
+
