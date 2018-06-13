9b169c3c43a180921c690d448fa3afa6d86054ca
maxsv0 <max.svistunov@gmail.com>
2018-06-13 22:52:34 +0200

    activationUrl fix

--- src/module/paper-bootstrap-wizard/paper-bootstrap-wizard.php
+++ src/module/paper-bootstrap-wizard/paper-bootstrap-wizard.php
@@ -1,10 +1,10 @@
 <?php
 
 // Include CSS for Paper Bootstrap Wizard
-msv_include(CONTENT_URL."/css/paper-bootstrap-wizard.css");
+msv_include(CONTENT_URL."/css/paper-bootstrap-wizard.css", $this->activationUrl);
 
 // Include JS for Paper Bootstrap Wizard
-msv_include(CONTENT_URL."/js/jquery.bootstrap.wizard.js");
-msv_include(CONTENT_URL."/js/paper-bootstrap-wizard.js");
+msv_include(CONTENT_URL."/js/jquery.bootstrap.wizard.js", $this->activationUrl);
+msv_include(CONTENT_URL."/js/paper-bootstrap-wizard.js", $this->activationUrl);
 
 
